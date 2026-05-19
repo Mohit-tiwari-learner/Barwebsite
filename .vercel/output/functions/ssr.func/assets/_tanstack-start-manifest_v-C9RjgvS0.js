21679e351ff98b0e817a9305021dacb2282c6023
VedantGiga <vedantvyas79@gmail.com>
2026-05-19 18:37:24 +0530

    chore: ignore entire .vercel directory instead of project.json specifically

--- .vercel/output/functions/ssr.func/assets/_tanstack-start-manifest_v-C9RjgvS0.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/LENOVO/OneDrive/Desktop/for videos/nightfall-narrative-main/nightfall-narrative-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-BfSh1zkC.js"] }, "/": { filePath: "C:/Users/LENOVO/OneDrive/Desktop/for videos/nightfall-narrative-main/nightfall-narrative-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-D1wxeS2E.js"] } }, clientEntry: "/assets/index-BfSh1zkC.js" });
-export {
-  tsrStartManifest
-};
--- .vercel/output/functions/ssr.func/assets/_tanstack-start-manifest_v-COElJ2mz.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/bar-website/Barwebsite-forked/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-B2qpXMuN.js"] }, "/": { filePath: "D:/bar-website/Barwebsite-forked/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-7x_1y55h.js"] } }, clientEntry: "/assets/index-B2qpXMuN.js" });
-export {
-  tsrStartManifest
-};
